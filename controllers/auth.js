10a2b316a92a97c1b957a4e82797e852a6833ee4
ahana0212 <ahana0212singh@gmail.com>
2025-05-27 21:19:03 +0530

    minor update

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,9 @@ exports.generate_otp_for_login = async (req, res) => {
   try {
     const { userName } = req.body;
     const PhoneNumber = userName;
+    console.log(userName);
     const user = await User.findOne({ PhoneNumber });
+    console.log(user);
     if (!user) return res.status(404).json({ error: "User not found" });
 
     // Generate a 6-digit OTP
@@ -22,6 +24,7 @@ exports.generate_otp_for_login = async (req, res) => {
 
     res.status(200).json({ message: "OTP sent successfully", success: true });
   } catch (err) {
+    console.log(err);
     res.status(400).json({ error: err.message });
   }
 };
